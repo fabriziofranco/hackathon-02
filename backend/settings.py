b1dade69de2d3e67552caf25dd6e03dbb26dd757
Christian Ledgard <39962750+christianledgard@users.noreply.github.com>
2021-01-14 09:57:06 -0500

    Revert "Features/jwt"

--- backend/settings.py
+++ backend/settings.py
@@ -128,8 +128,5 @@ STATIC_URL = '/static/'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    'PAGE_SIZE': 10
 }
