55190e823cfb69b3fdaa384e536b755a3cd23ee9
Christian <christian.ledgard@utec.edu.pe>
2021-01-18 16:21:47 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.0.2.2']
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.2.2', 'cosecha-api.herokuapp.com']
 
 AUTH_USER_MODEL = 'agricultores.User'
 
