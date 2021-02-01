0902046d93a083da3fad34d29e9dde6e493fd956
fabriziofranco <fabrizio.franco@utec.edu.pe>
2021-01-31 19:58:32 -0500

    Working

--- agricultores/api_urls.py
+++ agricultores/api_urls.py
@@ -4,5 +4,7 @@ from agricultores.views import *
 urlpatterns = [
     path('regions/', RegionFilterView.as_view()),
     path('districts/', DistrictFilterView.as_view()),
-    path('pubs/', PublishFilterView.as_view())
+    path('pubs/', PublishFilterView.as_view()),
+    path('compradores/', CompradorFilterView.as_view()),
+    path('agricultores/', AgricultorFilterView.as_view()),
 ]
