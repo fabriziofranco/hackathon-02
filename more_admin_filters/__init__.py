430b1a0c6f58a90a6801a4b8f47818bc670cbe96
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-09-19 01:28:02 -0500

    cleaning

--- more_admin_filters/__init__.py
+++
@@ -1,9 +0,0 @@
-VERSION = (1, 0)
-__version__ = ".".join(map(str, VERSION))
-
-
-from .filters import (
-    MultiSelectFilter, MultiSelectRelatedFilter, MultiSelectDropdownFilter,
-    MultiSelectRelatedDropdownFilter, DropdownFilter, ChoicesDropdownFilter,
-    RelatedDropdownFilter, BooleanAnnotationFilter
-)
--- more_admin_filters/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoAdminSelectfilterConfig(AppConfig):
-    name = 'django_more_admin_filters'
